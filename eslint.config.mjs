e8e46a036ad30d4d673366ff623ff55227306170
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-01-24 09:03:59 +0700

    batman

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120
+        }
+      ]
+    },
+    ignores: ['**/node_modules/', '**/dist/']
+  }
+]
