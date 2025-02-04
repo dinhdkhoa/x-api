af70d4d0ec86b3522316eb4744a63d5ac004508e
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-02-04 14:03:09 +0700

    feat: img resize with sharp

--- src/controllers/media.controllers.ts
+++ src/controllers/media.controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { MediaService } from '~/services/media.services'
 
 export const uploadImage = async (req: Request, res: Response) => {
-  const data = await MediaService.uploadFile(req, res)
-  res.json(data)
+  const data = await MediaService.uploadImage(req, res)
+  const { newName } = await MediaService.resizeImage(data)
+  res.json({ message: 'Image Upload Success', image: `http://localhost:4000/uploads/${newName}` })
 }
--- src/services/media.services.ts
+++ src/services/media.services.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from 'express'
-import formidable, { errors as formidableErrors } from 'formidable'
+import formidable, { File, errors as formidableErrors } from 'formidable'
 import path from 'path'
 import { UserRequest } from '~/models/schemas/user.schema'
 import fs from 'fs'
 import { error } from 'console'
 import { ErrorWithStatus } from '~/models/errors.model'
 import { HttpStatusCode } from '~/constants/HttpStatusCode.enum'
+import sharp from 'sharp'
 
 const uploadsFolderPath = path.resolve('uploads/images')
 
@@ -14,43 +15,47 @@ const uploadImage = async (req: Request, res: Response) => {
     uploadDir: uploadsFolderPath,
     maxFiles: 1,
     keepExtensions: true,
-    maxFileSize: 3000 * 1024, //300kb,
+    maxFileSize: 300 * 1024, //300kb,
     filter: ({ name, mimetype, ...part }) => {
       const valid = name === 'image' && Boolean(mimetype?.includes('image'))
       if (!valid) {
         //@ts-ignore
-        form.emit('error', new formidableErrors.default('file is Not an Image', 0, 400))
+        form.emit('error', new formidableErrors.default('file is Not an Image', 0, HttpStatusCode.UnsupportedMediaType))
       }
 
       return valid
     }
   })
 
-  return new Promise((resolve, reject) => {
+  return new Promise<File>((resolve, reject) => {
     return form.parse(req, (err, fields, files) => {
       if (err) {
         reject(err)
       }
-      if (!files.images) {
+      if (!files.image) {
         reject(new ErrorWithStatus({ message: 'File Empty', status: HttpStatusCode.BadRequest }))
       }
-      resolve(files)
+      resolve((files.image as unknown as File[])[0])
     })
   })
 }
 
-const UPLOAD_IMAGE_TEMP_DIR = path.resolve('uploads/images')
-const UPLOAD_VIDEO_TEMP_DIR = path.resolve('uploads/videos')
-export const initFolder = () => {
-  ;[UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_TEMP_DIR].forEach((dir) => {
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir, {
-        recursive: true // mục đích là để tạo folder nested
-      })
-    }
-  })
+const resizeImage = async (img: File) => {
+  const tempFilePath = img.filepath
+  const newName = img.newFilename.split('.')[0] + '.jpeg'
+  const resizedFilePath = path.join(uploadsFolderPath, newName)
+  sharp.cache(false)
+  const resizedImg = await sharp(tempFilePath).jpeg({ quality: 20 }).toFile(resizedFilePath)
+
+  fs.unlinkSync(tempFilePath)
+
+  return { ...resizedImg, newName }
 }
 
+const UPLOAD_IMAGE_TEMP_DIR = path.resolve('uploads/images')
+const UPLOAD_VIDEO_TEMP_DIR = path.resolve('uploads/videos')
+
 export const MediaService = {
-  uploadFile: uploadImage
+  uploadImage,
+  resizeImage
 }
